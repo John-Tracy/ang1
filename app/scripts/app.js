ce68a30ef09b91a0bcd9573ffebd9ea39f8f5c2c
John Tracy <johnmtracy133@hotmail.com>
2016-10-22 00:26:25 -0400

    app structure created

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ang1App
+ * @description
+ * # ang1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ang1App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ang1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ang1App
+ */
+angular.module('ang1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
